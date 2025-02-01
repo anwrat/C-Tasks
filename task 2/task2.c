208dadd4ada81471036ab8174bfdc5956b3d13df
anwrat <anweshrwt22@gmail.com>
2025-02-01 21:26:14 +0545

    User input for number of thread and iterations

--- task 2/task2.c
+++ task 2/task2.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <pthread.h>
 
+
+
 void* leibniz(void*arg){
 	
 }
 
 int main(){
+	int threadnum,iterations;
+	printf("Enter the number of iterations: ");
+	scanf("%d",&iterations);
+	printf("Enter the number of threads to make: ");
+	scanf("%d",&threadnum);
+	if(threadnum==0 || iterations==0){
+		printf("Number of threads and iterations must be greater than 0.\n");
+		return 1;
+	}
 	return 0;
 }
