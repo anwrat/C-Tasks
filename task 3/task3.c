f4589648c56f19d599be985327e9ca401869f53d
anwrat <anweshrwt22@gmail.com>
2025-02-03 19:17:23 +0545

    Merging files better lol

--- task 3/task3.c
+++ task 3/task3.c
@@ -4,6 +4,28 @@
 #include <stdlib.h>
 #include <math.h>
 
+void appendtofile(char fpath[]){
+	FILE *fp1,*flr;
+	fp1=fopen(fpath,"r");
+	if (fp1 == NULL) {
+        printf("Error opening file %s\n", fpath);
+        return;
+    }
+	flr=fopen("combinedPrimeData.txt","a");
+	if (flr == NULL) {
+        printf("Error opening output file combinedLR.txt\n");
+        fclose(fp1); 
+        return;
+    }
+	char ch;
+	while((ch=fgetc(fp1))!=EOF){
+		fputc(ch,flr);
+	}
+	printf("\nSuccessfully appended!!");
+	fclose(fp1);
+	fclose(flr);
+}
+
 int checkprime(int n){
 	if(n<2) return 0;
 	for(int i=2;i<=sqrt(num);i++){
@@ -13,9 +35,11 @@ int checkprime(int n){
 }
 
 int main(){
-	FILE *f1=fopen("PrimeData1.txt","r");
-	FILE *f2=fopen("PrimeData2.txt","r");
-	FILE *f3=fopen("PrimeData3.txt","r");
+	//Appending all 3 files to one
+//	appendtofile("PrimeData1.txt");
+//	appendtofile("PrimeData2.txt");
+//	appendtofile("PrimeData3.txt");
+	FILE *file=fopen("combinedPrimeData.txt","r");
 	FILE *prime=fopen("AllPrimeNumbers.txt",a);
 	if(file==NULL){
 		printf("Error opening file combinedLR\n");
