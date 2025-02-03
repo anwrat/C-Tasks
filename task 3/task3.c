53cf5cac201f49fbbb4c915f83c3dd887d5e533b
anwrat <anweshrwt22@gmail.com>
2025-02-03 20:52:04 +0545

    Almost finished task3, left for recheck

--- task 3/task3.c
+++ task 3/task3.c
@@ -4,6 +4,13 @@
 #include <stdlib.h>
 #include <math.h>
 
+typedef struct{
+	int start,end;
+}ThreadArgs;
+
+int *numbers,*primes,prime_count=0;
+pthread_mutex_t mutex;
+
 void appendtofile(char fpath[]){
 	FILE *fp1,*flr;
 	fp1=fopen(fpath,"r");
@@ -28,10 +35,21 @@ void appendtofile(char fpath[]){
 
 int checkprime(int n){
 	if(n<2) return 0;
-	for(int i=2;i<=sqrt(num);i++){
-		if(num%i==0) return 0;
+	for(int i=2;i<=sqrt(n);i++){
+		if(n%i==0) return 0;
 	}
 	return 1;
+}
+
+void *primechecker(void *arg){
+	ThreadArgs *args=(ThreadArgs*)arg;
+	for(int i=args->start;i<args->end;i++){
+		if(checkprime(numbers[i])){
+			pthread_mutex_lock(&mutex);
+			primes[prime_count++]=numbers[i];
+			pthread_mutex_unlock(&mutex);
+		}
+	}
 }
 
 int main(){
@@ -40,15 +58,61 @@ int main(){
 //	appendtofile("PrimeData2.txt");
 //	appendtofile("PrimeData3.txt");
 	FILE *file=fopen("combinedPrimeData.txt","r");
-	FILE *prime=fopen("AllPrimeNumbers.txt",a);
 	if(file==NULL){
 		printf("Error opening file combinedLR\n");
 		return 1;
 	}
 	char num[100];
-	int n,total;
-	while(fgets(num,sizeof(num),file)!=NULL){
-		n=atoi(num);
+	int total_numbers=0,temp,threadnum;
+	//Checking total numbers in the file
+	while(fscanf(file,"%d",&temp)==1){
+		total_numbers++;		
 	}
+	//Resetting the file pointer
+	rewind(file);
+	numbers = (int *)malloc(total_numbers * sizeof(int));
+    primes = (int *)malloc(total_numbers * sizeof(int));
+	//For storing all numbers in an array
+	int x=0;
+	while (fscanf(file, "%d", &numbers[x]) == 1) {
+        x++;
+    }
+    fclose(file);
+	printf("Enter the number of threads to use: ");
+	scanf("%d",&threadnum);
+	pthread_mutex_init(&mutex, NULL);
+	pthread_t* threads = malloc(threadnum * sizeof(pthread_t));
+	ThreadArgs *args = malloc(threadnum * sizeof(ThreadArgs));
+	int base_slice = total_numbers/ threadnum;
+    int remainder = total_numbers % threadnum;
+    int start = 0;
+    for (int i = 0; i < threadnum; i++) {
+        args[i].start = start;
+        args[i].end = start + base_slice;
+        if (i < remainder) {
+    		args[i].end += 1;
+		}
+        start = args[i].end; 
+    }
+    for (int i = 0; i < threadnum; i++) {
+        pthread_create(&threads[i], NULL, primechecker, &args[i]);
+    }
+    for (int i = 0; i < threadnum; i++) {
+        pthread_join(threads[i], NULL);
+    }
+	FILE *output = fopen("primes_output.txt", "w");
+    fprintf(output, "Total primes: %d\n", prime_count);
+    for (int i = 0; i < prime_count; i++) {
+        fprintf(output, "%d\n", primes[i]);
+    }
+    printf("Total numbers in the files: %d",total_numbers);
+    printf("\nPrime numbers in the files: %d",prime_count);
+    printf("\nPrime numbers written to file successfully!!");
+    fclose(output);
+    free(numbers);
+    free(primes);
+    free(threads);
+    free(args);
+    pthread_mutex_destroy(&mutex);
 	return 0;
 }
