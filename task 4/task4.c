e52ba47ead8c1298fad1e7089637e28a17d6592e
anwrat <anweshrwt22@gmail.com>
2025-02-04 07:05:09 +0545

    Trying task4

--- task 4/task4.c
+++ task 4/task4.c
@@ -1,21 +1,80 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
 #include "lodepng.h"
 
+unsigned char* input_image,*output_image;
+unsigned int h,w;
+
+typedef struct{
+	int start,stop;
+}ThreadArgs;
+
+void *gaussian_blur(void *arg){
+	ThreadArgs *args=(ThreadArgs*)arg;
+	for (int y = args->start; y < args->stop; y++) {
+        for (int x = 0; x < w; x++) {
+            double r = 0, g = 0, b = 0,a=0;
+            int count = 0;
+            for (int ky = -1; ky <= 1; ky++) {
+                for (int kx = -1; kx <= 1; kx++) {
+                    int nx = x + kx;
+                    int ny = y + ky;
+                    if (nx >= 0 && ny >= 0 && nx < w && ny < h) {
+                        int index = 4 * (ny * w + nx);
+                        r += input_image[index];
+                        g += input_image[index + 1];
+                        b += input_image[index + 2];
+                        a += input_image[index + 3];
+                        count++;
+                    }
+                }
+            }
+            int index = 4 * (y * w + x);
+            output_image[index] = (unsigned char)(r / count);
+            output_image[index + 1] = (unsigned char)(g / count);
+            output_image[index + 2] = (unsigned char)(b / count);
+            output_image[index + 3] = (unsigned char)(a / count);
+        }
+    }
+}
+
 int main(){
-	//Create a copy of array for applying gaussian blur
-	unsigned char* image;
-	unsigned int h,w,x,y;
-	lodepng_decode32_file(&image,&w,&h,"mario.png");
-	printf("The image is of size %d x %d",w,h);
-	printf("\nFor Gaussian Blur:");
-	printf("\nEnter the x-coordinate of the pixel: ");
-	scanf("%d",&x);
-	printf("\nEnter the y-coordinate of the pixel: ");
-	scanf("%d",&y);
-	if (x > w || y > h) {
-        printf("Number of pixels to change cannot exceed image dimensions.\n");
+	char *input="mario.png",*output="GaussianMario.png";
+	int threadnum;
+	if (lodepng_decode32_file(&input_image,&w,&h, input)) {
+        printf("Error loading image file %s\n", input);
         return 1;
     }
+	printf("The image is of size %d x %d",w,h);
+	printf("\nFor Gaussian Blur:");
+    output_image=(unsigned char*)malloc(4*w*h);
+    if (!output_image) {
+        printf("Error allocating memory for output image\n");
+        free(input_image);
+        return 1;
+    }
+    printf("\nEnter the number of threads: ");
+    scanf("%d",&threadnum);
+    pthread_t* threads = malloc(threadnum * sizeof(pthread_t));
+	ThreadArgs *args = malloc(threadnum * sizeof(ThreadArgs));
+	int rows_per_thread=h/threadnum;
+	for(int i=0;i<threadnum;i++){
+		args[i].start=i*rows_per_thread;
+		if (i == threadnum - 1) {
+            args[i].stop = h;
+        } else {
+            args[i].stop = (i + 1) * rows_per_thread;
+        }
+        pthread_create(&threads[i], NULL, gaussian_blur, &args[i]);
+	}
+	for (int i = 0; i < threadnum; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    lodepng_encode32_file(output, output_image, w, h);
+    free(input_image);
+    free(output_image);
+    free(threads);
+    free(args);
 	return 0;
 }
